9e81232e4b0749c42be051f198f865b23f7ea67b
Macfilho <macfilho2006@gmail.com>
2019-07-01 13:51:27 -0300

    controle de estoque , php com mysql
    baste ler o readme e executar o db.sql em um banco mysql

---
+++ bd.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(0);
+$valida[admin] = "admin";
+?>
+<?php
+$valida[maurofilho] = "macfilho31";
+?><?php
+$valida[teste] = "123456";
+?><?php
+$valida[admin] = "admin";
+?><?php
+$valida[maurofilho] = "macfilho31";
+?><?php
+$valida[MAUROFILHO] = "macfilho31";
+?><?php
+$valida[maurofilho] = "macfilho31";
+?>
